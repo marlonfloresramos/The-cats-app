0164750e10ba413393510b38050274dea1f34b0a
marlonfr <marlon.floresramos07@gmail.com>
2023-04-11 11:19:13 -0500

    Initial Commit

---
+++ The cats app/The_cats_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_cats_appApp.swift
+//  The cats app
+//
+//  Created by Marlon Gabriel Flores Ramos on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct The_cats_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
